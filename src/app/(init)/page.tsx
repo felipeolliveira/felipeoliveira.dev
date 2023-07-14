9e17f50e01ebe007527b084fdb6da23ae467b969
Felipe Oliveira <flpoliveira42@gmail.com>
2023-07-14 16:03:19 -0300

    shadow adjustments

--- src/app/(init)/page.tsx
+++ src/app/(init)/page.tsx
@@ -14,21 +14,23 @@ export default async function Home() {
 
       
       {user && (
-        <article className="bg-white/10 rounded-3xl text-slate-800 px-10 py-16 flex flex-col justify-center items-center max-w-xl backdrop-blur-md shadow-xl shadow-inner shadow-white/50">
-          <Image src={user.avatar_url} alt={user.name} width={180} height={180} objectFit="cover" className="rounded-full"/>
-          <h1 className="whitespace-nowrap font-bold text-4xl mt-12 text-slate-100">{user?.name}</h1>
-          <span className="text-sm text-slate-100/50 text-xl">Software Developer</span>
+        <span className="shadow-md rounded-3xl">
+          <article className="bg-white/5 rounded-3xl text-slate-800 px-10 py-16 flex flex-col justify-center items-center max-w-xl backdrop-blur-md shadow-inner shadow-white/50">
+            <Image src={user.avatar_url} alt={user.name} width={180} height={180} objectFit="cover" className="rounded-full"/>
+            <h1 className="whitespace-nowrap font-bold text-4xl mt-12 text-slate-100">{user?.name}</h1>
+            <span className="text-sm text-slate-100/50 text-xl">Software Developer</span>
 
-          <p className="text-center max-w-sm text-slate-100 leading-tight mt-12">{user.bio}</p>
+            <p className="text-center max-w-sm text-slate-100 leading-tight mt-12">{user.bio}</p>
 
-          <div className="flex gap-4 mt-10">
-            <Button as="a" target="_blank" href="https://github.com/felipeolliveira" icon={Icons.Github}>Github</Button>
-            <Button as="a" target="_blank" href="https://linkedin.com/in/felipeolliveira" icon={Icons.Linkedin}>Linkedin</Button>
-          </div>
-        </article>
+            <div className="flex gap-4 mt-10">
+              <Button as="a" target="_blank" href="https://github.com/felipeolliveira" icon={Icons.Github}>Github</Button>
+              <Button as="a" target="_blank" href="https://linkedin.com/in/felipeolliveira" icon={Icons.Linkedin}>Linkedin</Button>
+            </div>
+          </article>
+        </span>
       )}
 
-      {!user && ('carregando...')}
+      {!user && ('Carregando...')}
     </main>
   )
 }
